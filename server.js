58d3d61018139864c660425602b89ee7178db2df
InvMiqx <39607713+InvMiqx@users.noreply.github.com>
2021-03-08 16:40:19 -0500

    deviceNameLog

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const path = require('path');
 const cors = require('cors');
 const fetch = require('node-fetch');
 const mongoose = require('mongoose');
+const os = require('os');
 
 // app.use(cors());
 // app.options('*', cors());
 app.use(express.static(path.join(__dirname, 'public'))); //html n stuff on request
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 
 mongoose.connect(
   "mongodb+srv://JakeGinesin:acoolpassword@cluster0.0cp2n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -27,6 +29,14 @@ var postSchema = new mongoose.Schema({
 
 const Post = mongoose.model('Post', postSchema);
 
+var logSchema = new mongoose.Schema({
+  ip: String,
+  deviceName: String,
+  date: Date
+});
+
+const Log = mongoose.model('Log', logSchema);
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public','index.html'));
   console.log(req.ip);
@@ -40,6 +50,30 @@ app.listen(process.env.PORT || 3000, function(err){
 
 //api
 
+
+
+app.post('/api/logdata', async(req, res) => {
+  try{
+
+    let networkDataLog = os.networkInterfaces();
+    let ipLog = networkData['eth0'][0]['address'];
+
+    let deviceNameLog = os.hostname();
+
+    // Log.collection.insertOne({
+    //   ip: ipLog,
+    //   deviceName: deviceNameLog,
+    //   date: new Date()
+    // });
+
+    res.end(JSON.stringify(deviceNameLog));
+  }
+  catch(err){
+    console.log(err);
+  }
+
+});
+
 app.get('/api/getposts', async(req, res) => {
   try{
       let posts = await Post.find({}).sort({"date" : -1});
@@ -48,25 +82,6 @@ app.get('/api/getposts', async(req, res) => {
   catch(err){
     console.log(err);
   }
-});
-
-const os = require('os');
-
-
-app.post('/api/logdata', async(req, res) => {
-  try{
-
-    let networkData = os.networkInterfaces();
-    let deviceName = os.hostname();
-
-    // res.send(deviceName);
-
-    res.end(JSON.stringify(networkData["Eth0"][0]["address"]));
-  }
-  catch(err){
-    console.log(err);
-  }
-
 });
 
 app.post('/api/addpost', (req, res) => {
